e5115803e3abdad368a4d52bbbedcc794770d71d
klae01 <tspt2479@gmail.com>
2022-05-01 23:35:09 +0900

    candidate 3

--- hashvec/normal/__init__.py
+++ hashvec/normal/__init__.py
@@ -3,19 +3,12 @@ import jax.numpy as np
 @jax.jit
 def normalize(X):
     return X/np.linalg.norm(X, ord=2, axis = -1, keepdims=True)
+    
 @jax.jit
 def rotate(P, V):
+    P /= np.linalg.norm(P, ord=2, axis = -1, keepdims=True)
     θ = np.linalg.norm(V, ord=2, axis = -1, keepdims=True)
-    # limit = 0.01
-    # V[θ[..., 0] > limit] *= limit / θ[θ > limit][..., None]
-    # θ = np.minimum(θ, limit)
-    return P*np.cos(θ) + V/θ*np.sin(θ)
-    
-# @jax.jit
-# def cost(normal):
-#     X=np.triu(np.einsum("im,jm->ij", normal, normal), k=1)
-#     X=np.einsum("ij,ij", X, X) # np.square(X).sum()
-#     return X / (normal.shape[0] * (normal.shape[0]-1) / 2)
+    return P*np.cos(θ) + V*np.sinc(θ/np.pi)
 
 @jax.jit
 def cost(normal):
@@ -23,25 +16,36 @@ def cost(normal):
     X=(X.sum()-np.einsum("ii", X))
     return X / (normal.shape[0] * (normal.shape[0]-1))
 
-
-
 grad = jax.grad(cost)
 
-def uniform_normal_vector(n: int, m: int, steps: int = 200, learning_rate = 0.01):
+def uniform_normal_vector(n: int, m: int, steps: int = 500, learning_rate = 0.25):
     # n dimension 
     # m normal vector
     normal = normalize(jax.random.normal(jax.random.PRNGKey(123), (n, m)))
-    momentum = 0
+    β1 = 0.75
+    β2 = 0.9975
+    Ɛ = 1e-6
+    M = 0
+    S = 0
     for iters in range(steps):
         gradient = grad(normal)
-        ortho = gradient - normal * (normal@gradient.T).sum(axis = -1, keepdims = True)
 
-        momentum += (ortho - momentum) * 0.1
+        M += (1-β1) * (gradient - M)
+        S += (1-β2) * (np.square(gradient-S) - S) + Ɛ
+
+        eff = M / (1-β1**(1 + iters))
+        eff /= (S / (1-β2**(1 + iters)))**0.5 + Ɛ
+
+        eff -= np.einsum("ij,ik,ik->ij", normal, normal, eff)
+        debug = abs(np.einsum("ij,ij->i", normal, eff)).sum()
 
         step = (iters + 0.1) / (steps / 3) if iters < steps / 3 else (steps - iters) / (2 * steps / 3) * 0.9 + 0.1
-        step *= -momentum * learning_rate
+        step *= -eff * learning_rate
         
         normal = rotate(normal, step)
-        normal = normalize(normal)
+        # normal = normalize(normal)
         if iters % 10 == 0:
-            print(cost(normal), np.linalg.norm(normal, ord=2, axis = -1).mean(), abs(step).min(), abs(step).max())
+            print(cost(normal), np.linalg.norm(normal, ord=2, axis = -1).mean(), abs(step).min(), abs(step).max(), debug)
+
+# uniform_normal_vector(100, 3, 200, 0.007)
+# uniform_normal_vector(1000, 100, 2000, 0.5)
