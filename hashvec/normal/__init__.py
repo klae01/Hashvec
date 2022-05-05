08bcba82c6dc112fdc5db29b9e4ac540edcf9f9a
klae01 <tspt2479@gmail.com>
2022-05-05 11:43:18 +0900

    candidate 8 - outer product

--- hashvec/normal/__init__.py
+++ hashvec/normal/__init__.py
@@ -8,29 +8,34 @@ def normalize(X):
     return X/np.linalg.norm(X, ord=2, axis = -1, keepdims=True)
 
 @jax.jit
-def cost(EYE_MAT, normal):
-    convert = np.einsum("ijk,ik->ij", EYE_MAT, normal)
-    cost=np.square(np.einsum("im,jm->ij", convert, convert))
-    cost=(cost.sum()-np.einsum("ii", cost))
-    return cost / (normal.shape[0] * (normal.shape[0]-1))
+def rotate(P, V):
+    P /= np.linalg.norm(P, ord=2, axis = -1, keepdims=True)
+    θ = np.linalg.norm(V, ord=2, axis = -1, keepdims=True)
+    pos = P*np.cos(θ) + V*np.sinc(θ/np.pi)
+    return pos
 
-grad = jax.value_and_grad(cost, argnums=0)
+@jax.jit
+def cost(normal):
+    X=np.square(np.einsum("im,jm->ij", normal, normal))
+    X=(X.sum()-np.einsum("ii", X))
+    return X
+
+grad = jax.value_and_grad(cost)
 
 def uniform_normal_vector(n: int, m: int, steps: int = 500, learning_rate = 0.25):
     # n dimension 
     # m normal vector
-    EYE_MAT = np.identity(m)[None, :].repeat(n, axis = 0)
     normal = normalize(jax.random.normal(jax.random.PRNGKey(123), (n, m)))
-    β1 = 0.5
-    β2 = 0.999
-    Ɛ = 1e-12
+    β1 = 0.9
+    β2 = 0.9975
+    Ɛ = 1e-10
     M = 0
     S = 0
     for iters in range(steps):
         # Gradient
-        cost_v, grad_v = grad(EYE_MAT, normal)
-        gradient = -grad_v
-
+        cost_v, grad_v = map(lambda x: x/(n * (n-1)), grad(normal) )
+        gradient = np.einsum("ij,ik->ijk", normal, grad_v)
+        
         # Adabelief optimizer
         M += (1-β1) * (gradient - M)
         S += (1-β2) * (np.square(gradient-M) - S) + Ɛ
@@ -38,27 +43,23 @@ def uniform_normal_vector(n: int, m: int, steps: int = 500, learning_rate = 0.25
 
         eff = M / (1-β1**(1 + iters))
         eff /= ( S / (1-β2**(1 + iters)) )**0.5 + Ɛ
+        
+        eff = np.einsum("ij,ijk->ik", 1 / normal / m, eff)
+        eff -= np.einsum("ij,ik,ik->ij", normal, normal, eff)
 
+        # # clip norm 
+        # norm = np.linalg.norm(eff, ord = 2, axis = -1, keepdims = True)
+        # eff = np.where(norm > 0.1, eff / norm * 0.1, eff)
 
         # scheduler
         step = (iters + 0.1) / (steps / 3) if iters < steps / 3 else (steps - iters) / (2 * steps / 3) * 0.9 + 0.1
-        step *= learning_rate
+        step *= -learning_rate
         step *= eff
-
-        scale = abs(np.linalg.det(EYE_MAT+step))
-        step /= scale[..., None, None]
         
-        delta = np.einsum("ijk,ik->ij", step, normal)
-
-        # clip norm 
-        norm = np.linalg.norm(delta, ord = 2, axis = -1, keepdims = True)
-        delta = np.where(norm > 0.01, delta / norm * 0.01, delta)
-        
-        normal = normal / scale[..., None] + delta
-
+        normal = rotate(normal, step)
+        # normal = normalize(normal)
         if iters % 10 == 0:
-            print(cost_v, np.linalg.norm(normal, ord=2, axis = -1).mean(), (step).min(), (step).max(), abs(step).min(), abs(step).max())
-        normal = normalize(normal)
+            print(cost_v, np.linalg.norm(normal, ord=2, axis = -1).mean(), abs(step).min(), abs(step).max())
 
 # uniform_normal_vector(100, 3, 200, 0.007)
 # uniform_normal_vector(1000, 100, 2000, 0.5) # 0.009009007
